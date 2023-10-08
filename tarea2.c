7ef32d7cb5d8b880401ec36955814eb1c84867bc
Rena <raaguirre1@miuandes.cl>
2023-10-08 16:56:46 -0300

    added funcion para leer una linea especifica del archivo

--- tarea2.c
+++ tarea2.c
@@ -3,7 +3,7 @@
 #include<stdlib.h>
 #include<string.h>
 
-void printMatriz(char* matriz)
+void show_sudoku(char* matrix)
 {
     // funcion para printear el sudoku, sabemos que siempre sera de 9x9 
     int rows = 9;
@@ -11,25 +11,25 @@ void printMatriz(char* matriz)
     
     printf("    A B C   D E F   G H I \n");
     
-    for(int i = 0; i<rows; i++)
+    for (int i = 0; i<rows; i++)
     {   
         if ((i) % 3 == 0)
         {
             printf("  -------------------------\n");
         }
-        for(int j = 1; j<cols+1; j++)
+        for (int j = 1; j<cols+1; j++)
         {   
             if (j == 1)
             {
                 printf("%d | ", i+1);
             }
-            if (matriz[(j+(rows*i))-1] == '0')
+            if (matrix[(j+(rows*i))-1] == '0')
             {
-                printf("  ");
+                printf("0 ");
             } 
             else
             {
-                printf("%c ", matriz[(j+(rows*i))-1]);
+                printf("%c ", matrix[(j+(rows*i))-1]);
             }
             if (j % 3 == 0)
             {
@@ -41,12 +41,38 @@ void printMatriz(char* matriz)
             }           
         }
     }
-}
+    printf("  -------------------------\n");
 
+}
+void read_specific_line(char *file_name, int line_number, char *result_line)
+{   
+    //lee una linea especifica del archivo, denuevo sabemos que sera de 9x9 caracteres;
+    //recibimos un puntero string para almacernar el resultado final
+    //lo recibimos para poder manejar la memoria desde afuera de la funcion
+    FILE *file;
+    int max_characters = 83;
+    //83 es el max de characters que queremos leer,
+    //81 de la matriz + \n
+
+    file = fopen(file_name, "r");
+
+    if (file == NULL)
+    {
+        printf("Error leyendo el archivo.");
+    }
+    for(int i = 0; i < line_number; i++)
+    {
+        fgets(result_line, max_characters, file); 
+    }
+    fgets(result_line, max_characters, file); //este ultimo para dejarlo almacenado
+    fclose(file);
+}
 
 int main()
 {
-    char matriz[100] = "000200000060107400000060008000000100000010040000008000000000000000032000003890000";
-    printMatriz(matriz);
+    char matrix[100] = "";
+    read_specific_line("puzles.txt", 10, matrix);
+    printf("%s", matrix); 
+    show_sudoku(matrix);
 
 }
